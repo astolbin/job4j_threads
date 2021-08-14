585caf497acf7abf8e6463259de11582be352d1b
stolbin <stolbin@samsonpost.ru>
2021-08-14 09:52:17 +0300

    3. Junit тест для блокирующей очереди. [#68589]

--- src/main/java/ru/job4j/wait/SimpleBlockingQueue.java
+++ src/main/java/ru/job4j/wait/SimpleBlockingQueue.java
@@ -31,4 +31,8 @@ public class SimpleBlockingQueue <T>{
         notifyAll();
         return queue.poll();
     }
+
+    public synchronized boolean isEmpty() {
+        return queue.isEmpty();
+    }
 }
--- src/test/java/ru/job4j/wait/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/wait/SimpleBlockingQueueTest.java
@@ -2,6 +2,10 @@ package ru.job4j.wait;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.IntStream;
+
 import static org.junit.Assert.*;
 
 public class SimpleBlockingQueueTest {
@@ -36,5 +40,43 @@ public class SimpleBlockingQueueTest {
         consumer.join();
 
         assertEquals(3, (int)queue.poll());
+    }
+
+    @Test
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
+        Thread producer = new Thread(
+                () -> IntStream.range(0, 5).forEach(
+                        (n) -> {
+                            try {
+                                queue.offer(n);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                                Thread.currentThread().interrupt();
+                            }
+                        }
+                )
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+
+        assertEquals(Arrays.asList(0, 1, 2, 3, 4), buffer);
     }
 }
