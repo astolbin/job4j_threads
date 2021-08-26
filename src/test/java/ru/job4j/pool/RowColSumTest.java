4c2aa8a4ba0c26b4a7d6757c072a86bf2972275c
stolbin <stolbin@samsonpost.ru>
2021-08-26 07:53:04 +0300

    4. CompletableFuture [#361626] входные и ожидаемые данные инициализируются локально в тестах

--- src/test/java/ru/job4j/pool/RowColSumTest.java
+++ src/test/java/ru/job4j/pool/RowColSumTest.java
@@ -1,6 +1,5 @@
 package ru.job4j.pool;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.concurrent.ExecutionException;
@@ -9,33 +8,49 @@ import java.util.stream.IntStream;
 import static org.junit.Assert.*;
 
 public class RowColSumTest {
-    private final int[][] matrix = {
-            {1, 3, 5},
-            {2, 4, 6},
-            {7, 8, 9}
-    };
-    private final RowColSum.Sums[] expect = new RowColSum.Sums[matrix.length];
-
-    @Before
-    public void setUp() {
-        IntStream.range(0, matrix.length)
-                .forEach(i -> expect[i] = new RowColSum.Sums());
-
-        expect[0].setRowSum(9);
-        expect[1].setRowSum(12);
-        expect[2].setRowSum(24);
-        expect[0].setColSum(10);
-        expect[1].setColSum(15);
-        expect[2].setColSum(20);
-    }
-
     @Test
     public void whenSyncSum() {
-        assertEquals(expect, RowColSum.sum(matrix));
+        int[][] inputMatrix = {
+                {1, 3, 5},
+                {2, 4, 6},
+                {7, 8, 9}
+        };
+
+        RowColSum.Sums[] expectedSums = new RowColSum.Sums[inputMatrix.length];
+
+        IntStream.range(0, inputMatrix.length)
+                .forEach(i -> expectedSums[i] = new RowColSum.Sums());
+
+        expectedSums[0].setRowSum(9);
+        expectedSums[1].setRowSum(12);
+        expectedSums[2].setRowSum(24);
+        expectedSums[0].setColSum(10);
+        expectedSums[1].setColSum(15);
+        expectedSums[2].setColSum(20);
+
+        assertEquals(expectedSums, RowColSum.sum(inputMatrix));
     }
 
     @Test
     public void whenAsyncSum() throws ExecutionException, InterruptedException {
-        assertEquals(expect, RowColSum.asyncSum(matrix));
+        int[][] inputMatrix = {
+                {1, 3, 5},
+                {2, 4, 6},
+                {7, 8, 9}
+        };
+
+        RowColSum.Sums[] expectedSums = new RowColSum.Sums[inputMatrix.length];
+
+        IntStream.range(0, inputMatrix.length)
+                .forEach(i -> expectedSums[i] = new RowColSum.Sums());
+
+        expectedSums[0].setRowSum(9);
+        expectedSums[1].setRowSum(12);
+        expectedSums[2].setRowSum(24);
+        expectedSums[0].setColSum(10);
+        expectedSums[1].setColSum(15);
+        expectedSums[2].setColSum(20);
+
+        assertEquals(expectedSums, RowColSum.asyncSum(inputMatrix));
     }
 }
